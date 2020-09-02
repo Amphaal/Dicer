226e64a61eba8b21b00e0e455b065fba740e8895
Amphaal <guillaume.vara@gmail.com>
2020-09-03 00:35:24 +0200

    add appveyor script

---
+++ toolchains/clang-ci-appveyor.cmake
@@ -0,0 +1,7 @@
+
+SET(CMAKE_BUILD_TYPE Release)
+
+SET (CMAKE_C_COMPILER               "clang")	
+SET (CMAKE_CXX_COMPILER             "clang++")
+
+SET (CMAKE_CXX_FLAGS                "-D_WIN32_WINNT=0x0601")
